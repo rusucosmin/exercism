d77bede625d1f3891c23d38c3be1e55630750b2e
Cosmin Rusu <cr.rusucosmin@gmail.com>
2020-09-06 19:49:58 -0700

    Updated Bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,12 +1,19 @@
 defmodule Bob do
   def hey(input) do
     cond do
-      (Regex.match?(~r/^[ ]/, input) || String.length(input) == 0) -> "Fine. Be that way!"
-      (input == String.upcase(input) && Regex.match?(~r/[a-zA-Z]/, input) && String.last(input) == "?") -> "Calm down, I know what I'm doing!"
+      (String.length(String.trim(input)) == 0) -> "Fine. Be that way!"
+      (input == String.upcase(input) && String.last(input) == "?" && !contains_numbers(input)) -> "Calm down, I know what I'm doing!"
+      (input == String.upcase(input) && !contains_only_number(input)) -> "Whoa, chill out!"
       (String.last(input) == "?") -> "Sure."
-      (input == String.upcase(input) && Regex.match?(~r/[a-zA-Z]/, input)) -> "Whoa, chill out!"
-      (String.last(input) == "И") -> "Whoa, chill out!"
       true -> "Whatever."
     end
+  end
+
+  def contains_numbers(input) do
+    Regex.match?(~r/[0-9]/, input)
+  end
+
+  def contains_only_number(input) do
+    String.trim(String.replace(input, ~r/([0-9]|,|\?)/, "")) == ""
   end
 end
